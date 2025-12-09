019e47173ebbebb1b570ce0c6b401a7d389c7c08
Maria Luiza Rodrigues <86676797+maria-luiza-rodrigues-ti@users.noreply.github.com>
2025-12-09 00:22:35 +0000

    feat: update app title and layout structure; add Prettier dependency

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-
 export default function Home() {
   return (
-    <h1>Mimoo</h1>
+    <main className="h-screen w-screen">
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <h1>Bem-vinde ao Mimoo</h1>
+        <p>Aplicação para confirmação de chá de bebê de Iandê e Niara</p>
+      </div>
+    </main>
   );
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+  import eslintConfigPrettier from "eslint-config-prettier/flat";
+
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +15,7 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  eslintConfigPrettier,
 ]);
 
 export default eslintConfig;
